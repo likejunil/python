addb63e6a0f88bc4d22ec39118ab14100bc33664
june1 <likejunil@gmail.com>
2022-12-08 08:05:24 +0900

    contextlib 를 학습하라

---
+++ src/lecture/inflearn/level3/ch2_copy.py
@@ -0,0 +1,10 @@
+"""
+copy 에는 3가지 종류가 있다.
+. value ,reference (just assign)
+. shallow copy (first step element copy)
+. deep copy (all step element copy)
+
+# import copy
+# copy.copy()
+# copy.deepcopy()
+"""
---
+++ src/lecture/inflearn/level3/ch3_context.py
@@ -0,0 +1,10 @@
+"""
+자원(resource)를 안정적으로 할당 및 반환
+대표적으로 with 구문이 있다.
+
+. Contextlib
+. __enter__
+. __exit__
+
+"""
+
