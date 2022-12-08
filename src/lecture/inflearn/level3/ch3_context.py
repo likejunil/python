d8c84a940ebd2c014f66d987560d1d09a9bcebef
june1 <likejunil@gmail.com>
2022-12-08 21:22:05 +0900

    파이썬 언더바의 사용을 학습하라

--- src/lecture/inflearn/level3/ch3_context.py
+++ src/lecture/inflearn/level3/ch3_context.py
@@ -1,10 +1,10 @@
 """
-자원(resource)를 안정적으로 할당 및 반환
-대표적으로 with 구문이 있다.
+Contextlib 란 자원(resource)를 안정적으로 할당하고 반환하기 위해서 사용하는 방식이다.
+with .. as .. 구문으로 사용한다.
 
-. Contextlib
-. __enter__
-. __exit__
-
+1. 사용하는 방식은 크게 클래스 구현과
+    __enter__ 와 __exit__ 구현
+2. 함수 작성이 있다.
+    @contextlib.contextmanager 데코레이터와 yield 사용
 """
 
