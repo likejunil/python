e7011c8d5384651d8fe9290b6e5c0521cbfe07ab
권준일 <likejunil@gmail.com>
2022-12-14 07:53:27 +0900

    동시성과 병렬성을 학습하라

--- src/lecture/inflearn/level4/ch2_process.py
+++ src/lecture/inflearn/level4/ch2_process.py
@@ -25,17 +25,35 @@ print(share_value.value)
 share_list = Array('i', range(50))
 print(share_list.value)
 
+
+<< 4 >>
+from multiprocessing import Queue, Pipe
+q = Queue()
+q.put(message)
+message = q.get()
+
+p, c = Pipe()
+p.send(message)
+message = c.recv()
 """
 
-from multiprocessing import Process
+from multiprocessing import Process, current_process
+import os
 
 
-def proc_func():
-    pass
+def proc_func(a, b, c):
+    print(f'자식 프로세스, name=|{current_process().name}| pid=|{os.getpid()}| 시작')
+    print(f'a={a}, b={b}, c={c}')
+    print(f'자식 프로세스, name=|{current_process().name}| pid=|{os.getpid()}| 종료')
 
 
 def func1():
-    process = Process(name='준일', target=proc_func, args=(1,))
+    print(f'부모 프로세스, name=|{current_process().name}| pid=|{os.getpid()}| 시작')
+    p1 = Process(name='준일', target=proc_func, args=(1, 2, 3,))
+    p1.start()
+
+    p1.join()
+    print(f'부모 프로세스, name=|{current_process().name}| pid=|{os.getpid()}| 종료')
 
 
 if __name__ == '__main__':
---
+++ src/lecture/inflearn/level4/ch3_async_nonblock.py
@@ -0,0 +1,10 @@
+"""
+<< non-block, async >>
+서비스를 요청하는 쪽과 서비스를 처리하는 쪽이 존재한다.
+client, server
+block, non-block
+sync, async
+
+<< cpu-bound, io-bound >>
+
+"""
